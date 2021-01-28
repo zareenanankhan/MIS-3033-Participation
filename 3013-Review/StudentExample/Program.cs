8b99a81cc0e744c10b3761cf2aa5cb11361bcec6
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2021-01-28 14:37:45 -0600

    COMMIT ALL

---
+++ 3013-Review/StudentExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+/// <summary>
+/// @Author Zareen Anan Khan's Classwork
+/// </summary>
+
+namespace StudentExample
+{
+    class Program
+    {
+        private static IEnumerable<object> students;
+
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("studentdataRandom100Rows.csv");
+
+            List<Student> students = new List<Student>();
+
+            //Student s = new Student(5, "Joe", "Joe", "Blue");
+            //Student s1 = new Student();
+            //s1.FirstName = "Joe";
+            //s1.LastName = "Joe";
+
+            //foreach (var line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                var pieces = line.Split(',');
+                int id = (int)Convert.ToInt64(pieces[0]);
+
+                Student currentStudent = new Student(Convert.ToInt64(pieces[0], pieces[1], pieces[2], pieces[3]));
+                students.Add(currentStudent);
+
+            }
+
+            
+
+            Console.ReadLine();
+        }
+
+        private static void PrintStudentsWithFavoriteColor(string color)
+        {
+            foreach (var student in students)
+            {
+                if (student.FavoriteColor.ToLower() == color.ToLower())
+                {
+                    Console.WriteLine(student.CreateFullName());
+                }
+            }
+
+        }
+        private static void PrintStudentsWithFavoriteColor(string v, object color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 3013-Review/StudentExample/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentExample
+{
+    public class Student
+    {
+        private string MadeUpThingForExample;
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteColor { get; set; }
+
+        /// <summary> Default/Empty Constructor
+       
+        public Student()
+        {
+            ID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+
+        /// <summary> Overloaded constructor
+        
+
+        public Student(int id, string first, string last, string color)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            FavoriteColor = color;
+
+        }
+
+        // in the event that a student has an ID but no other detail is known, after an ID is given to us OU asks us to fill in the rest of the details.
+        public Student(int id)
+        {
+            ID = id;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+
+        }
+
+        public string CreateFullName()
+        {
+            //return LastName+", "+FirstName
+
+            return $"{LastName}, {FirstName}";
+        }
+
+
+    }
+
+
+}
