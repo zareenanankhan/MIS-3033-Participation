642fa7d666d2c3f945013906add0b451cbf4028a
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2021-02-11 14:56:36 -0600

    commit

---
+++ WPF1/WPF1/EntryForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WPF1
+{
+    class EntryForm
+    { 
+        public string Name { get; set;}
+        public string Address { get; set;}
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        {
+            string Name = string.Empty;
+            string Address = string.Empty;
+            int ZipCode = 00000;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Address}) ({ZipCode})";
+
+            
+        }
+    }
+}
---
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonsubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            EntryForm form = new EntryForm()
+            {
+                Name = textname.Text,
+                Address = textaddress.Text,
+                ZipCode = Convert.ToInt32(textzip.Text),
+
+            };
+
+            listbox.Items.Add(form);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EntryForm selected = (EntryForm)listbox.SelectedItem;
+        }
+    }
+}
