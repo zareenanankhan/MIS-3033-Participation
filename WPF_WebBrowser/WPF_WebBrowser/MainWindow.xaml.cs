1d50e58e87ef03c64b6ed5ebf9ec54a502e69955
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2021-02-18 14:03:35 -0600

    in class participation

---
+++ WPF_WebBrowser/WPF_WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_WebBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttongo_Click(object sender, RoutedEventArgs e)
+        {
+            string query = textquery.Text;
+
+            WebBrowserWindow google = new WebBrowserWindow(); //shows three different ways to pass the query
+            //google.Query = query;
+            google.Search();
+            google.Search(query);
+            google.ShowDialog();
+        }
+    }
+}
---
+++ WPF_WebBrowser/WPF_WebBrowser/WebBrowserWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_WebBrowser
+{
+    /// <summary>
+    /// Interaction logic for WebBrowserWindow.xaml
+    /// </summary>
+    public partial class WebBrowserWindow : Window // three methods shows three different ways to pass the query
+    {
+        public string Query { get; set; }
+        public WebBrowserWindow()
+        {
+            InitializeComponent();
+
+            webbrowser.Navigate($"https://www.google.com");
+        }
+
+        public void Search(string q)
+        {
+            Query = q;
+            webbrowser.Navigate($"https://www.google.com/search?q={q}");
+        }
+
+        public void Search()
+        {
+            webbrowser.Navigate($"https://www.google.com/search?q={Query}");
+        }
+    }
+}
