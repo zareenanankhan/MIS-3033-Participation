57d652762afbb4d4787a840849c2af9770f4d054
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2021-02-22 19:50:40 -0600

    progress

---
+++ WPF_Classes_Participatipn/WPF_Classes_Participatipn/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes_Participation
+{
+    class Address
+    {
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+
+        public Address()
+        {
+            StreetNumber = 0;
+            StreetName = string.Empty;
+            State = string.Empty;
+            City = string.Empty;
+            ZipCode = 0;
+
+        }
+
+        public Address(int streetnumber, string streetname, string state, string city, int zipcode)
+        {
+            StreetNumber = streetnumber;
+            StreetName = streetname;
+            State = state;
+            City = city;
+            ZipCode = zipcode;
+        }
+    }
+}
---
+++ WPF_Classes_Participatipn/WPF_Classes_Participatipn/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes_Participation
+{
+    
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+        public double GPA { get; set; }
+        public Address Address { get; set; }
+
+        public Student()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Major = string.Empty;
+            GPA = 0.0;
+            Address = new Address(); // =null;
+
+        }
+
+        public Student(string firstName, string lastName, string major, double gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Major = major;
+            GPA = gpa;
+        }
+
+        public string CalculateDistinction()
+        {
+            string distinction;
+            if (GPA >= 3.80)
+            {
+                distinction = "Summa Cum Laude";
+            }
+            else if (GPA >= 3.60)
+            {
+                distinction = "Magna Cum Laude";
+            }
+            else if (GPA > -3.40)
+            {
+                distinction = "Cum Laude";
+            }
+            else
+            {
+                distinction = "No special distinction";
+            }
+
+            return distinction;
+
+        }
+
+        public void SetAddress(int streetNumber, string streetName, string state, string city, int zipcode)
+        {
+            Address = new Address(streetNumber, streetName, state, city, zipcode);
+            // Address.StreetNumber=streetnumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, {Major}, {CalculateDistinction()}";
+        }
+    }
+}
+
