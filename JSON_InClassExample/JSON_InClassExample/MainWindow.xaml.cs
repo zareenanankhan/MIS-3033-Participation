3b93d6336db0ab96c97f8c9d7f2efb91f835a16a
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2021-02-23 14:23:51 -0600

    commit

---
+++ JSON_InClassExample/JSON_InClassExample/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_InClassExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //https://rickandmortyapi.com/api/character
+
+            using (var client =new HttpClient())
+            {
+                string jsondata = client.GetStringAsync("https://rickandmortyapi.com/api/character").Result;
+
+                RickMortyAPI api = JsonConvert.DeserializeObject<RickMortyAPI>(jsondata);
+
+                foreach (var character in api.results)
+                {
+                    listcharacters.Items.Add(character);
+                }
+
+            }
+        }
+
+        private void listcharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedcharacter = (character)listcharacters.SelectedItem;
+            imagecharacter.Source = new BitmapImage(new Uri(selectedcharacter.image));
+        }
+    }
+}
---
+++ JSON_InClassExample/JSON_InClassExample/RickMortyAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_InClassExample
+{
+    public class RickMortyAPI
+    {
+        public Info info {get;set;}
+        public List<character> results { get; set; }
+    }
+
+    public class character
+    {
+        public string name { get; set; }
+        public string image { get; set; }
+        public string url { get; set; }
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+    }
+}
