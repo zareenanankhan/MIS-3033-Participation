3f5855af1b0a37e5f2f24995f55bf9f983a936d1
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2021-02-25 13:49:11 -0600

    progress

---
+++ JSON_Pokemon/JSON_Pokemon/AllPokemonAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Pokemon
+{
+    public class AllPokemonAPI
+    {
+        public List<ResultObject> results {get;set;}
+    }
+
+    public class ResultObject
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ JSON_Pokemon/JSON_Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AllPokemonAPI api;
+            string url = "https://pokeapi.co/api/v2/pokemon?limit=1200";
+
+
+            using (var client=new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+                api = JsonConvert.DeserializeObject<AllPokemonAPI>(json);
+            }
+
+            foreach(var item in api.results.OrderBy(x =>x.name).ToList())
+            {
+                listpokemon.Items.Add(item);
+
+            }
+           
+                
+            
+        }
+    }
+}
