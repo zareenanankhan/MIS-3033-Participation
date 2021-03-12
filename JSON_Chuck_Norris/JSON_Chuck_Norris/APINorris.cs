4a7a4229f1c87ff864542155b3695a6ec5bee323
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2021-03-11 18:59:36 -0600

    commit

---
+++ JSON_Chuck_Norris/JSON_Chuck_Norris/APINorris.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Chuck_Norris
+{
+    public class APINorris
+    {
+        
+        public string value { get; set; }
+
+    }
+}
---
+++ JSON_Chuck_Norris/JSON_Chuck_Norris/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Chuck_Norris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string norrisurl = "https://api.chucknorris.io/jokes/random?category";
+        string categoriesurl = "https://api.chucknorris.io/jokes/categories";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           
+            string categoriesurl = "https://api.chucknorris.io/jokes/categories";
+            List<string> categorieslist = new List<string>();
+            using (var client=new HttpClient())
+            {
+
+                string categorydata = client.GetStringAsync(categoriesurl).Result;
+
+                categorieslist= JsonConvert.DeserializeObject<List<string>>(categorydata);
+                
+                foreach (string category in categorieslist)
+                {
+                    combobox.Items.Add(category);
+                }
+
+                combobox.Items.Add("All");
+            }
+
+        }
+
+        private void buttonselect_Click(object sender, RoutedEventArgs e)
+        {
+
+            string selectedcategory = combobox.SelectedItem.ToString();
+
+            APINorris joke = new APINorris();
+
+            string url1 = $"https://api.chucknorris.io/jokes/random?category={selectedcategory}";
+
+            string url2 = $"https://api.chucknorris.io/jokes/random";
+
+            if (selectedcategory == "All")
+            {
+                using (var client = new HttpClient())
+                {
+                    string json = client.GetStringAsync(url2).Result;
+                    joke = JsonConvert.DeserializeObject<APINorris>(json);
+
+                }
+            }
+            else
+            {
+                using (var client = new HttpClient())
+                {
+                    string json = client.GetStringAsync(url1).Result;
+                    joke = JsonConvert.DeserializeObject<APINorris>(json);
+                }
+            }
+
+            joketext.Text = joke.value;
+        }
+    }
+}
