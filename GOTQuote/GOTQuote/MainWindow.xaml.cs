bdf333493052e5b51dc67084c26f9423c52d5bfd
Zareen Khan <Zareen.A.Khan-1@ou.edu>
2021-03-11 19:50:36 -0600

    commit

---
+++ GOTQuote/GOTQuote/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GOTQuote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           
+
+           
+        }
+
+        private void buttonget_Click(object sender, RoutedEventArgs e)
+        {
+            gotquote api = new gotquote();
+            string url = " https://got-quotes.herokuapp.com/quotes";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+                api = JsonConvert.DeserializeObject<gotquote>(json);
+            }
+
+            textquote.Text = api.ToString();
+        }
+    }
+}
---
+++ GOTQuote/GOTQuote/gotquote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GOTQuote
+{
+    public class gotquote
+    {
+        public string quote { get; set; }
+        public string character { get; set; }
+
+        public override string ToString()
+        {
+            return $"Quote: {quote} \n Who said it: {character}.";
+        }
+    }
+}
